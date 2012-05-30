21fefc570cf957871dacd8676af61a254773b89d
Christopher Haster <chaster@utexas.edu>
2012-05-29 22:15:08 -0500

    Filled out buffer class

--- Buffer.cpp
+++ Buffer.cpp
@@ -54,29 +54,46 @@ Buffer& Buffer::operator>>(sf::Uint64 & d) {
     d |= *(index++);
 }
 
-/*Buffer& Buffer::operator>>(float & d) {
-    
+Buffer& Buffer::operator>>(float & d) {
+	sf::Uint32 temp;
+    *this >> temp;
+	d = reinterpret_cast<float>(temp);
 }
 
 Buffer& Buffer::operator>>(double & d) {
-    
+    sf::Uint64 temp;
+    *this >> temp;
+	d = reinterpret_cast<double>(temp);
 }
 
-Buffer& Buffer::operator>>(char*) {
-    
+Buffer& Buffer::operator>>(char * d) {
+   	while (index < limit) {	
+		if (!*index) {
+			index++;
+			break;
+		}
+
+		*(d++) = *(index++);
+	}
+
+	*d = 0;
 }
 
-Buffer& Buffer::operator>>(std::string&) {
-    
+Buffer& Buffer::operator>>(sf::String & d) {
+	Uint32 temp;
+	d.clear();
+	while (index+4 <= limit) {
+		*this >> temp;
+		if (!temp) break;
+		d += temp;
+	}
+	d += 0;
 }
 
-Buffer& Buffer::operator>>(sf::String&) {
-    
+Buffer& Buffer::operator>>(Address & a) {
+    this* >> a.address;
+	this* >> a.port;
 }
-
-Buffer& Buffer::operator>>(Address&) {
-    
-}*/
 
     
 Buffer& Buffer::put(const char * d, size_t s) {
@@ -116,13 +133,41 @@ Buffer& Buffer::operator<<(sf::Uint64 d) {
     *(index++) = d;
 }
     
-/*    Buffer& operator<<(float);
-    Buffer& operator<<(double);
-    Buffer& operator<<(const char*);
-    Buffer& operator<<(const std::string&);
-    Buffer& operator<<(const sf::String&);
-    Buffer& operator<<(const Address&);
-};*/
+Buffer& Buffer::operator<<(float d) {
+    *this << reinterpret_cast<sf::Uint32>(d);
+}
+
+Buffer& Buffer::operator<<(double) {
+	*this << reinterpret_cast<sf::Uint64>(d);
+}
+
+Buffer& Buffer::operator<<(const char * d) {
+	while (index < limit) {	
+		if (!*d) {
+			*(index++) = 0;
+			break;
+		}
+
+		*(d++) = *(index++);
+	}
+}
+
+Buffer& Buffer::operator<<(const sf::String & d) {
+	sf::String::ConstIterator i = d.begin();
+	while (index+4 <= limit) {
+		if (i == d.end()) {
+			*this << (sf::Uint32)0;
+			break;
+		}
+		*this << *i;
+		++i;
+	}
+}
+
+Buffer& Buffer::operator<<(const Address & a) {
+	*this << a.address;
+	*this << a.port;
+}
 
 } // comm
 
