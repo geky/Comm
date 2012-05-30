99ac755fd2bf93523c4f8dedb86e920f5dd74f84
Christopher Haster <chaster@utexas.edu>
2012-05-30 00:14:45 -0500

    Added buffer class

--- Buffer.cpp
+++ Buffer.cpp
@@ -1,61 +1,131 @@
 
 #include "comm.hpp"
+
 //TODO make a buffer class
+
 namespace comm {
 
-template<> inline void to_buffer(sf::Uint8 n, unsigned char* b) {
-    *b=n;
+Buffer::Buffer():
+        data(0), limit(0), index(0) {}
+Buffer::Buffer(const Buffer& b):
+        data(b.data), limit(b.limit), index(0) {} 
+Buffer::~Buffer() {}
+
+// Network write/read operations
+Buffer& Buffer::get(char * d, size_t s) {
+    assert(index+s <= limit);
+    memcpy(d,index,s);
+    index += s;
 }
     
-template<> inline void to_buffer(sf::Uint16 n, unsigned char* b) {
-    b[0] = n>>8;
-    b[1] = n;  
+Buffer& Buffer::operator>>(bool & d) {
+    assert(index < limit);
+    d = (*(index++) != 0);
 }
 
-template<> inline void to_buffer(sf::Uint32 n, unsigned char* b) {
-    b[0] = n>>24;
-    b[1] = n>>16;
-    b[2] = n>> 8;
-    b[3] = n;
+Buffer& Buffer::operator>>(sf::Uint8 & d) {
+    assert(index < limit);
+    d = *(index++);
 }
 
-template<> inline void to_buffer(sf::Uint64 n, unsigned char* b) {  
-    b[0] = n>>56;
-    b[1] = n>>48;
-    b[2] = n>>40;
-    b[3] = n>>32;
-    b[4] = n>>24;
-    b[5] = n>>16;
-    b[6] = n>> 8;
-    b[7] = n;
+Buffer& Buffer::operator>>(sf::Uint16 & d) {
+    assert(index+2 <= limit);
+    d  = *(index++); d<<=8;
+    d |= *(index++);
 }
 
-template<> inline sf::Uint8  from_buffer(unsigned char* b) {
-    return *b;
+Buffer& Buffer::operator>>(sf::Uint32 & d) {
+    assert(index+4 <= limit);
+    d  = *(index++); d<<=8;
+    d |= *(index++); d<<=8;
+    d |= *(index++); d<<=8;
+    d |= *(index++);
 }
 
-template<> inline sf::Uint16 from_buffer(unsigned char* b) {
-    return b[0]<<8 | b[1];
+Buffer& Buffer::operator>>(sf::Uint64 & d) {
+    assert(index+8 <= limit);
+    d  = *(index++); d<<=8;
+    d |= *(index++); d<<=8;
+    d |= *(index++); d<<=8;
+    d |= *(index++); d<<=8;
+    d |= *(index++); d<<=8;
+    d |= *(index++); d<<=8;
+    d |= *(index++); d<<=8;
+    d |= *(index++);
 }
 
-template<> inline sf::Uint32 from_buffer(unsigned char* b) {
-    return b[0]<<24 | 
-           b[1]<<16 |
-           b[2]<< 8 |
-           b[3];
-}
-
-template<> inline sf::Uint64 from_buffer(unsigned char* b) {
-    sf::Uint64 temp =  b[0];
-    temp = (temp<<8) | b[1];
-    temp = (temp<<8) | b[2];
-    temp = (temp<<8) | b[3];
-    temp = (temp<<8) | b[4];
-    temp = (temp<<8) | b[5];
-    temp = (temp<<8) | b[6];
-    temp = (temp<<8) | b[7];
+/*Buffer& Buffer::operator>>(float & d) {
     
-    return temp;
 }
+
+Buffer& Buffer::operator>>(double & d) {
+    
+}
+
+Buffer& Buffer::operator>>(char*) {
+    
+}
+
+Buffer& Buffer::operator>>(std::string&) {
+    
+}
+
+Buffer& Buffer::operator>>(sf::String&) {
+    
+}
+
+Buffer& Buffer::operator>>(Address&) {
+    
+}*/
+
+    
+Buffer& Buffer::put(const char * d, size_t s) {
+    assert(index+s <= limit);
+    memcpy(index,d,s);
+    index += s;    
+}
+    
+Buffer& Buffer::operator<<(bool d) {
+    assert(index < limit);
+    *(index++) = d;    
+}
+
+Buffer& Buffer::operator<<(sf::Uint8 d) {
+    assert(index+2 <= limit);
+    *(index++) = d>>8; 
+    *(index++) = d;
+}
+
+Buffer& Buffer::operator<<(sf::Uint16 d) {
+    assert(index+4 <= limit);
+    *(index++) = d>>24; 
+    *(index++) = d>>16;
+    *(index++) = d>>8;
+    *(index++) = d;
+}
+
+Buffer& Buffer::operator<<(sf::Uint64 d) {
+    assert(index+4 <= limit);
+    *(index++) = d>>56; 
+    *(index++) = d>>48;
+    *(index++) = d>>40;
+    *(index++) = d>>32;
+    *(index++) = d>>24; 
+    *(index++) = d>>16;
+    *(index++) = d>>8;
+    *(index++) = d;
+}
+    
+/*    Buffer& operator<<(float);
+    Buffer& operator<<(double);
+    Buffer& operator<<(const char*);
+    Buffer& operator<<(const std::string&);
+    Buffer& operator<<(const sf::String&);
+    Buffer& operator<<(const Address&);
+};*/
 
 } // comm
+
+
+
+
--- Buffer.hpp
+++ Buffer.hpp
@@ -4,6 +4,53 @@
 
 namespace comm {
 
+class Buffer {
+public:
+    Buffer();
+    Buffer(const Buffer&);
+    virtual ~Buffer();
+    
+    //use with caution
+    char * data;
+    char * index;
+    char * limit;
+    
+    Buffer& get(char*, size_t);
+    
+    Buffer& operator>>(bool&);
+    Buffer& operator>>(sf::Int8&);
+    Buffer& operator>>(sf::Uint8&);
+    Buffer& operator>>(sf::Int16&);
+    Buffer& operator>>(sf::Uint16&);
+    Buffer& operator>>(sf::Int32&);
+    Buffer& operator>>(sf::Uint32&);
+    Buffer& operator>>(sf::Int64&);
+    Buffer& operator>>(sf::Uint64&);
+    Buffer& operator>>(float&);
+    Buffer& operator>>(double&);
+    Buffer& operator>>(char*);
+    Buffer& operator>>(std::string&);
+    Buffer& operator>>(sf::String&);
+    Buffer& operator>>(Address&);
+
+    Buffer& put(const char*, size_t);
+    
+    Buffer& operator<<(bool);
+    Buffer& operator<<(sf::Int8);
+    Buffer& operator<<(sf::Uint8);
+    Buffer& operator<<(sf::Int16);
+    Buffer& operator<<(sf::Uint16);
+    Buffer& operator<<(sf::Int32);
+    Buffer& operator<<(sf::Uint32);
+    Buffer& operator<<(sf::Int64);
+    Buffer& operator<<(sf::Uint64);
+    Buffer& operator<<(float);
+    Buffer& operator<<(double);
+    Buffer& operator<<(const char*);
+    Buffer& operator<<(const std::string&);
+    Buffer& operator<<(const sf::String&);
+    Buffer& operator<<(const Address&);
+};    
 
 }
 
